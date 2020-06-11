bd67f0f08613c40eb0cb961c43d6781bb41f0f55
Ramesh88806 <57959335+Ramesh88806@users.noreply.github.com>
2020-06-11 02:43:32 +0200

    class client.py

--- client.py
+++ client.py
@@ -1,6 +1,12 @@
 import asyncio
 import sys
-
+class client_class:
+    def __init__(self, server_ip, server_port, list, result):
+        """Initilizing the variables"""
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.client_command_list = list
+        self.result = result
 def main():
     host = '127.0.0.1'
     port = 8888
