71ea26bbf6d7d23baa5f6832aeb3d9f5d1af97df
akhila8888 <66680678+akhila8888@users.noreply.github.com>
2020-06-12 18:05:35 +0200

    update list and login function in server_main

--- server_main.py
+++ server_main.py
@@ -3,7 +3,7 @@ import sys
 import time
 class server_class:
     """Creating server class"""
-    def __init__(self,reader,writer,client_list,logined_list,temporary_clients,read_file_data,result):
+    def __init__(self, reader, writer, client_list, logined_list, temporary_clients, read_file_data, result):
         """Initializing the variables"""
         self.reader = reader
         self.writer = writer
@@ -12,17 +12,62 @@ class server_class:
         self.temporary_clients = temporary_clients
         self.read_file_data = read_file_data
         self.result = result
-    async  def register_account(self,user_name,pass_word,privilages,addr):
-        """register account"""
-        if len(user_name) == 0 :
+    async def login_account(self, user_name, pass_word, addr):
+        """login account"""
+        count = 0
+        if self.logined_list[addr] == 0:
+            for i in range(len(self.client_list)):
+                user_name_match = self.client_list[i][0]
+                pass_word_match = self.client_list[i][1]
+                if (user_name == user_name_match and pass_word == pass_word_match):
+                    message = 'login successful'
+                    self.result.append(message)
+                    message = message.encode()
+                    tuple = (user_name, pass_word)
+                    self.logined_list[tuple] = 1
+                    self.temporary_clients = addr
+                    self.logined_list[addr] = 1
+                    try:
+                        count = count+1
+                        self.writer.write(message + '\n'.encode())
+                        break
+                    except:
+                        pass
+                    try:
+                        path = os.getcwd()
+                        try:
+                            os.chdir(user_name)
+                        except OSError:
+                            pass
+                    except OSError:
+                        pass
+            if count == 0:
+                message = 'login failed'
+                self.result.append(message)
+                message = message.encode()
+                try:
+                    self.writer.write(message + '\n'.encode())
+                except:
+                    pass
+        else:
+            message = 'logined already'
+            self.result.append(message)
+            message = message.encode()
             try:
-                message = 'user_name should not be empty'
-                self.result.append(message) 
-                message = message.encode()   
                 self.writer.write(message + '\n'.encode())
             except:
                 pass
-        elif(len(pass_word) == 0) :
+    async  def register_account(self, user_name, pass_word, privilages, addr):
+        """register account"""
+        if len(user_name) == 0:
+            try:
+                message ='user_name should not be empty'
+                self.result.append(message)
+                message = message.encode()
+                self.writer.write(message + '\n'.encode())
+            except:
+                pass
+        elif len(pass_word) == 0:
             try:
                 message = 'user_name should not be empty'
                 self.result.append(message) 
@@ -30,13 +75,13 @@ class server_class:
                 self.writer.write(message + '\n'.encode())
             except:
                 pass
-        elif(privilages == 'admin' or privilages == 'user'):
+        elif privilages == 'admin' or privilages == 'user':
             count = 0
-            client =  ( user_name , pass_word ,privilages)
+            client = (user_name, pass_word, privilages)
             for i in range(len(self.client_list)):
                 user_name_match = self.client_list[i][0]
                 pass_word_match = self.client_list[i][1]
-                if( user_name == user_name_match and pass_word == pass_word_match):
+                if (user_name == user_name_match and pass_word == pass_word_match):
                     count = count + 1
             if count == 0:
                 val = 0
@@ -57,7 +102,7 @@ class server_class:
                     message = message.encode()    
                     try:
                         self.writer.write(message + '\n'.encode())
-                        self.logined_list [addr]  = 1
+                        self.logined_list[addr] = 1
                     except:
                         pass
                     self.client_list.append(client)
@@ -78,11 +123,43 @@ class server_class:
                     self.writer.write(message + '\n'.encode())
                 except:
                     pass
-        elif (privilages != 'admin' and privilages != 'user' ):
+        elif (privilages != 'admin' and privilages != 'user'):
             message = 'privilages must be either admin or user'
             self.result.append(message) 
             message = message.encode()    
             try:
                 self.writer.write(message + '\n'.encode())
             except:
-                pass        
+                pass
+
+    async def list_files(self):
+        """list files"""
+        nw_list = []
+        try:
+            path = os.getcwd()
+            list = os.listdir(path)
+            string = ''
+            for i in range(len(list)):
+                size = os.path.getsize(list[i])
+                date = time.ctime(os.path.getctime(list[i]))
+                data = (list[i], size, date)
+                nw_list.append(data)
+            for i in range(len(nw_list)):
+                for j in range(len(nw_list[i])):
+                    if j == 0:
+                        string = string + 'Name_of_file = ' + nw_list[i][j] + ' '
+                    elif j == 1:
+                         string = string + 'size_of_file = ' + str(nw_list[i][j]) + 'bytes  '
+                    elif j == 2:
+                         string = string + 'Date_of_file and time_of_creation_of_file = ' + nw_list[i][j] + ' '
+                string = string + '\n'
+            try:
+                new_msg = string.encode()
+                self.result.append(True)
+                self.writer.write(new_msg + '\n'.encode())
+            except Exception as e:
+                # print('client connection disconnected')
+                pass
+        except OSError:
+            self.result.append(False)
+            print('os error')
