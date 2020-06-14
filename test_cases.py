3f6301ea466f01b8cd25bffdc3adfdabc80ccff4
Saidattu14 <66754678+Saidattu14@users.noreply.github.com>
2020-06-14 17:19:19 +0200

    updating list file and readfile

--- test_cases.py
+++ test_cases.py
@@ -5,12 +5,12 @@ from assign3_client import client_class
 class servertesting(unittest.TestCase):
 
     def setUp(self):
-        """Initializing the variables"""
+        """here we can setup the commands"""
 
         pass
 
     def test_register(self):
-        """testing the register in directory """
+        """here we can test the register function"""
         obj = server_class([], [], [], {}, {}, {}, [])
         asyncio.run(obj.register_account('vinay', '$vinay123', 'admin', '121.0.0.1'))
         asyncio.run(obj.register_account('vinay', '$vinay123', 'admin', '121.0.0.1'))
@@ -21,7 +21,7 @@ class servertesting(unittest.TestCase):
                            ]
         self.assertListEqual(obj.result, expected_results)
     def test_login(self):
-        """testing the login in directory"""
+        """here we can test the login function"""
         obj = server_class([], [], [], {}, {}, {}, [])
         client = ('vinay', '$vinay123', 'admin')
         obj.logined_list['121.0.0.2'] = 0
@@ -50,10 +50,26 @@ class servertesting(unittest.TestCase):
         asyncio.run(obj.change_folder('sai'))
         asyncio.run(obj.change_folder('ramesh'))
         expected_results = ['Folder created successfully',
-                            'Folder changed successfully'
+                            'Folder changed successfully',
                             'No such Folder_name exits in the directory'
                             ]
         self.assertListEqual(obj.result, expected_results)
+    def test_list_files(self):
+        """testing the list files in directory"""
+        obj = server_class([], [], [], {}, {}, {}, [])
+        asyncio.run(obj.list_files())
+        expected_result = [True]
+        self.assertListEqual(obj.result, expected_result)
+    def test_write_file(self):
+        """testing the write files in directory"""
+        obj = server_class([], [], [], {}, {}, {}, [])
+        asyncio.run(obj.write_file('sample.txt', 'saidattu'))
+        asyncio.run(obj.write_file('sample.txt', 'is a good'))
+        expected_results = ['New_file is created and content succesfully written in the file',
+                            'content succesfully written in the file'
+                           ]
+        self.assertListEqual(obj.result, expected_results)
         
- if __name__ == '__main__':
+        
+if __name__ == '__main__':
     unittest.main()
