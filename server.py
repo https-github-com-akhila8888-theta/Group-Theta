a0765086f13d960adda20bb53a09ab82a7178b16
Ramesh88806 <57959335+Ramesh88806@users.noreply.github.com>
2020-06-14 20:26:51 +0200

    update server docstrings

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""importing modules"""
 import asyncio
 import queue
 from server_main import server_class
@@ -9,7 +10,15 @@ queue_data = queue.Queue()
 result = []
 async def new_echo_handle(reader, writer):
     server = server_class(reader, writer, client_list, logined_list,\
-                          temporary_clients, read_file_data, result)
+        temporary_clients, read_file_data, result)
+    """attributes
+    reader: which the client is used to read a data from server
+    writer: client is used to write the data from the server
+    client_list: which is used to print the current clients in the server
+    logined_list: which is used to print the current login list from the server
+    temporary_clients: it will display the current clients in the server
+    read_file: which is used to read the file from the server
+    result: which is used to print the result"""
     addr = writer.get_extra_info('peername')
     logined_list[addr] = 0
     message = f"{addr} is connected !!!!"
@@ -17,114 +26,114 @@ async def new_echo_handle(reader, writer):
     while True:
         try:
             data = await reader.read(100)
-            new_msg = data.decode()
-            print(new_msg)
+            print(data)
             queue_data.put(data)
             while queue_data.empty() == 0:
                 queue_data.get()
                 message = data.decode().split()
                 if message[0] == 'login':
-                    """login here"""
+                    # here we can login from cilent to server
                     if len(message) != 3:
                         try:
-                            new_msg = 'Error occured in syntax of login\
-                                        data It must login user_name password'
+                            new_msg = 'Error occured in syntax of login data'\
+                            'It must login user_name password'
                             new_msg = new_msg.encode()
                             writer.write(new_msg + '\n'.encode())
-                        except:
+                        except Exception:
                             print('client connection disconnected')
                     else:
                         addr = writer.get_extra_info('peername')
                         await server.login_account(message[1], message[2], addr)
                 elif message[0] == 'register':
-                    """ before login we have to  register"""
+                    #we have to register from client to server"""
                     if len(message) != 4:
                         try:
-                            new_msg = 'Error occured in syntax of register data\
-                                        It must login user_name password privilages'
+                            new_msg = 'Error occured in syntax of register data'\
+                            'It must login user_name password privilages'
                             new_msg = new_msg.encode()
                             writer.write(new_msg + '\n'.encode())
-                        except:
+                        except Exception:
                             pass
                     else:
                         addr = writer.get_extra_info('peername')
                         await server.register_account(message[1], message[2], message[3], addr)
                 elif message[0] == 'quit':
-                    """Quit"""
+                    #Quit
                     try:
                         print('client disconnected')
                         break
-                    except:
+                    except Exception:
                         pass
                 elif message[0] == 'create_folder':
-                    """Create folder"""
+                    #we can Create a folder from client to server
                     if len(message) != 2:
                         try:
-                            new_msg = 'Error occured in the format of create_folder\
-                                         It must be create_folder and Folder_name'
+                            new_msg = 'Error occured in the format of create_folder'\
+                            'It must be create_folder and Folder_name'
                             new_msg = new_msg.encode()
                             writer.write(new_msg + '\n'.encode())
-                        except:
+                        except Exception:
                             pass
                     else:
                         await server.create_folder(message[1])
                 elif message[0] == 'read_file':
-                    """ reading the file"""
+                     #we can read the file from client to server"""
                     if len(message) != 2:
                         try:
-                            new_msg = 'Error occured in the format of read_file \
-                                         It must be read_file and file_name'
+                            new_msg = 'Error occured in the format of read_file'\
+                            'It must be read_file and file_name'
                             new_msg = new_msg.encode()
                             writer.write(new_msg + '\n'.encode())
-                        except:
+                        except Exception:
                             pass
                     else:
                         await server.read_file(message[1])
                 elif message[0] == 'write_file':
-                    """Writing the file"""
+                    # we can Write the file from client to server
+                    print('yes')
                     if len(message) != 3:
                         try:
-                            new_msg = 'Error occured in the format of write_file\
-                                         It must be write_file and file_name and data'
+                            new_msg = 'Error occured in the format of write_file'\
+                            'It must be write_file and file_name and data'
                             new_msg = new_msg.encode()
                             writer.write(new_msg + '\n'.encode())
-                        except:
+                        except Exception:
                             pass
                     else:
                         await server.write_file(message[1], message[2])
                 elif message[0] == 'change_folder':
-                    """Changing the directory"""
+                    #we can replace the existing folder with new folder
                     if len(message) != 2:
                         try:
-                            new_msg = 'Error occured in the format of change_folder\
-                                         It must be change_folder and name_to_be_changed'
+                            new_msg = 'Error occured in the format of change_folder'\
+                            'It must be change_folder and name_to_be_changed'
                             new_msg = new_msg.encode()
                             writer.write(new_msg + '\n'.encode())
-                        except:
+                        except Exception:
                             pass
                     else:
                         await server.change_folder(message[1])
                 elif message[0] == 'list':
-                    """List"""
+                    # List is used to show existing folders from the directory
                     if len(message) != 1:
                         try:
-                            new_msg = 'Error occured in the format of list of files\
-                                                   it should be just list'
+                            new_msg = 'Error occured in the format of list of files'\
+                            'it should be just list'
                             new_msg = new_msg.encode()
                             writer.write(new_msg + '\n'.encode())
-                        except:
+                        except Exception:
                             pass
                     else:
                         await server.list_files()
-                elif message[0] == 'delete' :
-                    """Deleting the files"""
+                elif message[0] == 'delete':
+                    #Deleting the user
                     if len(message) != 3:
                         try:
-                            new_msg = 'Error occured in the format of delete It must be\
-                                          delete and user_name and admin_password'
+                            new_msg = 'Error occured in the format of delete'\
+                            'It must be delete and user_name and admin_password'
                             new_msg = new_msg.encode()
                             writer.write(new_msg + '\n'.encode())
-                        except:
+                        except Exception:
                             pass
                     else:
                         addr = writer.get_extra_info('peername')
@@ -134,14 +143,14 @@ async def new_echo_handle(reader, writer):
                         new_msg = 'Invalid command is entered and try again'
                         new_msg = new_msg.encode()
                         writer.write(new_msg + '\n'.encode())
-                    except:
+                    except Exception:
                         print('Connection disconnected')
         except:
             addr = writer.get_extra_info('peername')
             logined_list[addr] = 0
             print('connection closed', addr)
             break
-    writer.close
+    writer.close()
 async def main():
     host = '127.0.0.1'
     port = 8888
